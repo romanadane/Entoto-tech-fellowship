7d970ba723b34b03ee81b8fe8faa538df01a8718
romanadane <romi.adane9941@gmail>
2019-06-09 03:24:10 -0700

    day2 exercise

---
+++ week1/day2.js
@@ -0,0 +1,13 @@
+var weatherTemperature = 40 {
+console.log(whether temprature)
+if(whether temprature <10) {
+    console.log("it is cold")
+    }
+else if(whether temprature >20) {
+  console.log("it is warm")
+}
+else if(whether temprature >10 && whether temprature < 20){
+  console.log("it is nice out side")
+} else{
+  console.log(weather Temperature)
+} }
